6eeada10d3586fe10d586039a5fb35fdc191cd3b
Sebastian <crsebastian_el_capo_2016@hotmail.com>
2024-02-09 15:31:27 -0400

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import {resolve} from 'node:path'
+import dts, {type PluginOptions} from 'vite-plugin-dts'
+
+
+const pluginConfigDts : PluginOptions= {rollupTypes: true}
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), dts(pluginConfigDts)],
+  root: './',
+  build: {
+    outDir : 'lib',
+    
+    lib: {
+      entry: resolve(__dirname, 'src/components/index.tsx'),
+      name: 'My first components library',
+      // the proper extensions will be added
+      formats: ['es','umd'],
+      fileName: 'index',
+    },
+    copyPublicDir : false,
+    minify: true,
+  },
+})
